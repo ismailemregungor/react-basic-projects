8b93376251b70e467c81a2ae19b82ed5f81a528d
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-09-21 00:11:27 +0300

    Initial commit

---
+++ react-router/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header/Header.jsx';
+import Main from './Components/Main/Main.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
